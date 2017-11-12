1ec7e8619a0299d03af4efc91ef1f807f1ed5776
firebird11 <hbgassel@gmail.com>
2017-11-12 12:07:56 +0100

    defconfig back to los
    Change-Id: I4bd3b15e03b5c95dd612a0609586d10fe3076822

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ BOARD_CUSTOM_BT_CONFIG := device/lge/d851/bluetooth/vnd_g3.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := crdroid_d851_defconfig
+TARGET_KERNEL_CONFIG := lineageos_d851_defconfig
 
 # NFC
 BOARD_NFC_CHIPSET := pn547
